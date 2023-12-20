3ef38688297ccd8f82cd90b2f1c205a5d02e5e9f
unknown <yeonghak77@akvdi.com>
2023-12-20 17:36:21 +0900

    test print GCP

--- google/api/googleTrendTopTerms.py
+++ google/api/googleTrendTopTerms.py
@@ -2,15 +2,14 @@ import os
 from google.cloud import bigquery
 
 # Secret
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', '{}')
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', '{}')
 
 # Query Parameter
 start_diff = os.environ.get('START_DIFF', '1')
 end_diff = os.environ.get('END_DIFF', '0')
 
 # test print
-print(os.environ)
-print('os2: ' + os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 print('start: ' + start_diff)
 print('end: ' + end_diff)
 
